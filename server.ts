1550a735e1697259a807befe6971c09d10b17766
Apurva1993 <richa.pathak1993@gmail.com>
2020-08-16 01:18:28 +0530

    Revert "Using __dirname"
    This reverts commit 88ae7ddf1925854c186d8ce43a628f61615f241e.

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(__dirname, 'dist/Space-X-program');
+  const distFolder = join(process.cwd(), 'dist/Space-X-program');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
